a376468f83e3ba5c022cbaca0104f1c78157dc5a
Joshua García <josshuagarciia000@gmail.com>
2019-10-05 09:10:18 -0700

    Fixed navbar #22 Added safearea

--- lib/v2/widgets/components/navbar.dart
+++ lib/v2/widgets/components/navbar.dart
@@ -18,43 +18,46 @@ class VocabularyBuilderNavbar extends StatelessWidget implements PreferredSizeWi
   Widget build(BuildContext context) {
 
     final Size screenSize = MediaQuery.of(context).size;
+    final bool isSmall = screenSize.width <= 479;
+    final int by = isSmall ? 5 : 10;
     final double size = Platform.isIOS 
     ? screenSize.width * 0.66
     : screenSize.width * 0.56;
 
-    final double top = -(screenSize.width * 0.126);
-    final double appBarTop = size / 2 + top - IconTheme.of(context).size / 2;
+    
+    final double appBarTop = size / by;
     final double twentysSix = 26;
 
-    return Padding(
-      padding: EdgeInsets.only(
-        left: twentysSix, 
-        right: twentysSix,
-        bottom: twentysSix,
-        top: appBarTop
-      ), // <- change that
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[   
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.only(
+          left: twentysSix, 
+          right: twentysSix,
+          bottom: twentysSix,
+          top: appBarTop
+        ),// <- change that
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[   
 
-          InkWell(
-            child: Icon(Icons.arrow_back),
-            onTap: Navigator.of(context).pop,
-          ),
+            InkWell(
+              child: Icon(Icons.arrow_back),
+              onTap: Navigator.of(context).pop,
+            ),
 
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: twentysSix), // <- change 
-            child: Text(
-              title,
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: twentysSix), // <- change 
+              child: Text(
+                title,
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-
-        ],
+          ],
+        ),
       ),
     );
   }
